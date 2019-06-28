fecee5f420f69182b7196f8ffb3399d6a609ee5b
Jorge Rosal <yortrosal@gmail.com>
2019-06-28 20:51:26 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const decompress = require('decompress');
+const decompressUnzip = require('decompress-unzip');
+
+const decodeBase64ToBinary = (base64String) => {
+    input   = new Buffer.from(base64String, 'base64');
+    decode  = input.toString('binary'); // decode to binary
+    return decode;
+};
+
+const extractZip = (base64String, dist) => new Promise((resolve, reject) => {
+    let binaryString = decodeBase64ToBinary(base64String);
+    fs.writeFile(dist, binaryString, {encoding: 'binary'}, err => {
+        if (err) reject(err)
+        else resolve('file saved!')
+});
+
+module.exports = extractZip;
